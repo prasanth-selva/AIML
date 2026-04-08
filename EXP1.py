9bd5c1bdb85af008d7de459a09ded0fcf00d96d3
prasanth-selva <prasanthselvaraj1511@gmail.com>
2026-04-08 09:18:00 +0530

    bfs&dfs

--- EXP1.py
+++ EXP1.py
@@ -5,19 +5,37 @@ graph = {
     'D': [],
     'E': [],
     'F': [],
+    'G': []
     }
 
 def bfs(start):
-    visited = []
-    queue = [start]
-
-    while queue:
-        node = queue.pop(0)
-
-        if node not in visited:
-            print(node, end =" ")
-            visited.append(node)
-        
-            for neighbor in graph[node]:
-                queue.append(neighbor)
+	visited = []
+	queue = [start]
+	
+	while queue:
+		node = queue.pop(0)
+		if node not in visited:
+			print(node, end =" ")
+			visited.append(node)
+							
+			for neighbour in graph[node]:
+				queue.append(neighbour)
+print("BFS\n")
 bfs('A')
+
+
+def dfs(node, visited = None):
+	if visited is None:
+		visited = []
+
+	if node not in visited:
+		print(node, end =" ")
+		visited.append(node)
+		
+
+		for neighbor in graph[node]:
+			dfs(neighbor,visited)  
+
+
+print("DFS\n")
+dfs('A')
